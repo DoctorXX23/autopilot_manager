f7956b47d692e089fc34e919c341edc4affbed93
fury1895 <yannick@auterion.com>
2022-02-10 11:17:04 +0100

    Custom Action: Add gimbal control examples

---
+++ data/example/custom_action/custom_action_scripts/gimbal_controls.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+#############################################################################
+#
+#   Copyright (c) 2022 Auterion AG. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+# 3. Neither the name PX4 nor the names of its contributors may be
+#    used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#############################################################################
+
+
+"""
+.. automodule:: gimbal-controls
+   :platform: Unix
+   :synopsis:
+       Script for controlling the gimbal
+   :members:
+
+.. codeauthor:: Yannick Fuhrer <yannick@auterion.com>
+"""
+
+import argparse
+import asyncio
+from mavsdk import System
+from mavsdk.gimbal import ControlMode
+
+async def run():
+    # Parse CLI arguments
+    parser = argparse.ArgumentParser(description="Script for controlling the gimbal")
+    parser.add_argument('--forward', help="Set gimbal into forward position", action="store_true")
+    parser.add_argument('--down', help="Set gimbal into down position", action="store_true")
+    args = parser.parse_args()
+
+    # Init drone
+    drone = System()
+    await drone.connect(system_address="udp://:14591")
+
+    if args.forward:
+        # take control over gimbal
+        await drone.gimbal.take_control(ControlMode.PRIMARY)
+        # control gimbal
+        await drone.gimbal.set_pitch_and_yaw(0, 0)
+        # release control
+        await drone.gimbal.release_control()
+
+    elif args.down:
+        # take control over gimbal
+        await drone.gimbal.take_control(ControlMode.PRIMARY)
+        # control gimbal
+        await drone.gimbal.set_pitch_and_yaw(-90, 0)
+        # release control
+        await drone.gimbal.release_control()
+
+if __name__ == '__main__':
+    # Start the main function
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
